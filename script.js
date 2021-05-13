81e9a4e946c395981790d9fd25414a42be5df628
Douglas de Oliveira <douglasdeoliveiradd@gmail.com>
2021-05-13 10:34:25 -0300

    finish

--- script.js
+++ script.js
@@ -69,14 +69,23 @@ function scrollToSection(event) {
 }
 initScrollSuave();
 
-const sections = document.querySelectorAll('.js-scroll');
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
 
-function animaScroll() {
-    section.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        if(sectionTop < 0 ) {
-         section.classList.add('ativo');
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+            })
         }
-    })
+        animaScroll()
+        window.addEventListener('scroll', animaScroll);
+    }
 }
-window.addEventListener('scroll', animaScroll);
+initAnimacaoScroll();
