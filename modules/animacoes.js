008ecac533ce01159b56e826489330c72f189091
Douglas de Oliveira <douglasdeoliveiradd@gmail.com>
2021-06-20 21:23:28 -0300

    scroll-suave

--- modules/animacoes.js
+++ modules/animacoes.js
@@ -9,7 +9,8 @@ if(tabMenu.length && tabContent.length) {
         tabContent.forEach((section) => {
         section.classList.remove('ativo');
         });
-    tabContent[index].classList.add('ativo');
+    console.log();
+    tabContent[index].classList.add('ativo', tabContent[index].dataset.anime);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -42,36 +43,10 @@ function initAccordion() {
 }
 initAccordion();
 
-function initScrollSuave() {
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
-    event.preventDefault();
-    const  href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-    });
-
-    //  forma alternativa
-    //  const topo = section.offsetTop;
-    //  window.scrollTo({
-    //  top: topo,
-    //  behavior: 'smooth',
-    // });
-}
-
-    linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-    });
-}
-initScrollSuave();
 
 function initAnimacaoScroll() {
     const sections = document.querySelectorAll('.js-scroll');
-
     if (sections.length) {
         const windowMetade = window.innerHeight * 0.6;
 
