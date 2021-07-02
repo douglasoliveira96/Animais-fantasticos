b8b3a27edd2943587f62be95270804bca8c8fef5
Douglas de Oliveira <douglasdeoliveiradd@gmail.com>
2021-07-02 19:11:05 -0300

    Funcionamento

--- js/script.js
+++ js/script.js
@@ -7,10 +7,7 @@ import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
-
-
-
-
+import initFuncionamento from './modules/funcionamento.js';
 
 
 initScrollSuave();
@@ -22,4 +19,4 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
-
+initFuncionamento();
